c20e8ed212994c7d1415af5a4c96d9771dcb9b95
GeneXu <genexu1117@gmail.com>
2018-08-20 00:54:37 +0800

    update youtube config

--- src/configs/youtubeConfig.js
+++ src/configs/youtubeConfig.js
@@ -1,7 +1,11 @@
 const youtubeConfig = {
   key: 'YOUTUBE_API_KEY',
   tags: [
-    { primaryTag: '厭世', secondaryTag: 'Outsider', shortcut: 'O', playlistId: 'PLWP5j0tk8f8bVqnShI15v_vgXwNk-l-Zh' }
+    { primaryTag: '勵志', secondaryTag: 'Inspirational', shortcut: 'I', playlistId: 'PLWP5j0tk8f8aDz3HvSy5I5moBLzo8kCaP' },
+    { primaryTag: 'FridayNight', secondaryTag: 'FirdayNight', shortcut: 'F', playlistId: 'PLWP5j0tk8f8YO0XDiSP4_92wZzvFAasjt' },
+    { primaryTag: '慵懶', secondaryTag: 'Lazy', shortcut: 'L', playlistId: 'PLWP5j0tk8f8bSC7WxA5ek7Gp2tCfqC3rP' },
+    { primaryTag: '厭世', secondaryTag: 'Outsider', shortcut: 'O', playlistId: 'PLWP5j0tk8f8bVqnShI15v_vgXwNk-l-Zh' },
+    { primaryTag: '老靈魂', secondaryTag: 'OldSoul', shortcut: 'OS', playlistId: 'PLWP5j0tk8f8b4_Q-XGluGIliTIcfXEjvn' },
   ]
 }
 
